9604112e3a09687040c7038e7156137b99499375
MarioSalcedo207068 <mario.salcedo207068@potros.itson.edu.mx>
2024-07-24 20:31:19 -0700

    print Report function implemented

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {crawlPage} = require('./crawl.js')
+const {printReport} = require('./report.js')
 //const { error } = require("console")
 
 async function main(){
@@ -16,10 +17,10 @@ async function main(){
 
     console.log(`starting crawl${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
-
-    for(const page of Object.entries(pages)){                            
-        console.log(page)
-    }
+    printReport(pages)
+//    for(const page of Object.entries(pages)){                            
+//        console.log(page)
+//   }
 }
 
 main()
